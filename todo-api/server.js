bd0285fd6966b9b3ab4cc126abd8d932e851cdb9
zackluckyf <fanning.34@gmail.com>
2016-11-06 23:17:30 -0500

    setup rest endpoint

--- todo-api/server.js
+++ todo-api/server.js
@@ -2,14 +2,30 @@
 
     'use strict';
 
-    var express = require('express');
-    var app = express();
-
+    let express = require('express');
+    let app = express();
     const PORT = process.env.PORT || 3000;
+    let todos = [{
+        id: 1,
+        description: 'Meet mom for lunch',
+        completed: false
+    }, {
+        id: 2,
+        description: 'Go to marked',
+        completed: false
+    }];
 
     app.get('/', function(req, res) {
         res.send('Todo API Root');
     });
+
+    app.get('/todos', function(req, res) {
+        res.json(todos);
+    });
+
+    // app.get('/todos/:id', function(req, res) {
+    //     res.json(todos.id);
+    // });
 
     app.listen(PORT, function() {
         console.log(`express server started on port: ${PORT}`);
