8263c0a1db69065d9ef2df718030f162a3f38cc6
zackluckyf <fanning.34@gmail.com>
2016-11-09 19:18:05 -0500

    Added a put method

--- todo-api/server.js
+++ todo-api/server.js
@@ -67,6 +67,37 @@
         }
     });
 
+    // PUT /todos/:id
+    app.put('/todos/:id', function(req, res) {
+        var todoId = parseInt(req.params.id, 10);
+        var matchedTodo = _.findWhere(todos, {
+            id: todoId
+        });
+        var body = _.pick(req.body, 'description', 'completed');
+        var validAttributes = {};
+
+        if (!matchedTodo) {
+            res.status(404).json({
+                "error": "no todo found with that id"
+            });
+        }
+
+        if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+            validAttributes.completed = body.completed;
+        } else if (body.hasOwnProperty('completed')) {
+            return res.status(400).send();
+        }
+
+        if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+            validAttributes.description = body.description;
+        } else if (body.hasOwnProperty('description')) {
+            return res.status(400).send();
+        }
+
+        _.extend(matchedTodo, validAttributes);
+        res.json(matchedTodo);
+    });
+
     app.listen(PORT, function() {
         console.log(`express server started on port: ${PORT}`);
     });
